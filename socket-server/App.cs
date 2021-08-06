47464508a1e3b3b803f20c1b6211116160b4d92d
UnRunDead <me@pscl.dev>
2021-08-06 21:42:05 +0200

    created csharp project

---
+++ socket-server/App.cs
@@ -0,0 +1,7 @@
+﻿namespace Shortcuts_Helper_Server {
+    public class App {
+        public string ProcessName { get; set; }
+        public string Name { get;set; }
+        public Shortcut[] Shortcuts { get; set; }
+    }
+}
---
+++ socket-server/Program.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using dotenv.net;
+using dotenv.net.Utilities;
+using WebSocketSharp.Server;
+
+namespace Shortcuts_Helper_Server {
+    class Program {
+        static void Main() {
+            DotEnv.Load();
+            
+            var port = EnvReader.GetIntValue("PORT");
+            var wssv = new WebSocketServer(port);
+            wssv.AddWebSocketService<ShortcutBehavior>("/shortcuts");
+            wssv.Start();
+            
+            Console.ReadKey(true);
+            wssv.Stop();
+        }
+    }
+}
