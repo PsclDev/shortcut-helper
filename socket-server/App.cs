cd8cc6f5922ea5e1adca6073ba3d5322ecf58c69
PsclDev <me@pscl.dev>
2022-07-01 22:56:12 +0200

    chore: removed old backend

--- socket-server/App.cs
+++
@@ -1,7 +0,0 @@
-﻿namespace Shortcuts_Helper_Server {
-    public class App {
-        public string ProcessName { get; set; }
-        public string Name { get;set; }
-        public Shortcut[] Shortcuts { get; set; }
-    }
-}
